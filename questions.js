942d6a45e17cd03b74429bd3242a9c4fabd61ea7
Amanda Foster <mandolinfoster@gmail.com>
2019-10-20 14:53:55 -0400

    Added questions for code quiz

---
+++ questions.js
@@ -0,0 +1,31 @@
+{
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "CSS is the language that _____ your HTML",
+    choices: ["styles", "reads", "hosts", "alerts"],
+    answer: "styles"
+  },
+  {
+    title: "Front-end development describes:",
+    choices: ["Saving user's input", "Styling a website's interface", "Managing all projects on a team", "None of the above"],
+    answer: "Styling a website's interface"
+  },
+  {
+    title: "DOM is an acronym for what?:",
+    choices: ["Display original material", "Denote order manipulation", "Discography of Macklemore", "Document object model"],
+    answer: "Document object model"
+  },
+  {
+    title: "Javascript makes websites _____",
+    choices: ["Hyper Taco Model Lady", "Hyper Tools Marketing Language", "Hypertext Markup Language", "Hyper Technique Model Language"],
+    answer: "Hypertext Markup Language"
+  };
+
